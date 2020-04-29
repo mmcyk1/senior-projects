fe41b79655169045d33b4fb8700f3d72ee2a2f9f
mmcyk1 <57729198+mmcyk1@users.noreply.github.com>
2020-04-29 12:16:59 -0600

    Update Sketch #2.ino

--- Sketch #2.ino
+++ Sketch #2.ino
@@ -61,4 +61,15 @@ void loop()
       }
     }
   }
+  {
+    val=Serial.read();
+    if(val=='s')
+    {
+      for (pos = 180; pos <= 0; pos -= 1) 
+      { 
+       servo13.write(pos);              
+       delay(15); 
+      }
+    }
+  } 
 }
